e8f7af14059a1479a5c6abc2f7c6b1c29a185677
Christopher <Cocopelly255@gmail.com>
2019-04-27 18:22:21 -0700

    Setting up directory structure.

--- server.js
+++ server.js
@@ -1,13 +1,4 @@
-"use strict";
+'use strict';
 
-var express = require('express');
-var app = express();
-
-app.get('/', function(req, res) {
-  res.send("Hello, World");
-});
-
-var server = app.listen(3000, function() {
-  var port = server.address().port;
-  console.log("example app listening on port %s", port);
-})
+var app = require('./config/lib/app');
+server = app.start();
